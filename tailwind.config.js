9a7859d7563b51ff02cc347f3f036d36e8593e48
Aris Nk✯ <aristotenkomba@gmail.com>
2026-05-31 04:45:27 +0000

    fix: set lang to fr and install tailwind css properly

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'syne': ['Syne', 'sans-serif'],
+        'jakarta': ['Plus Jakarta Sans', 'sans-serif'],
+        'dm-sans': ['DM Sans', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
